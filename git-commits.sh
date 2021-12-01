3dd84fb8c6f0553a0cce05dc4437971c609f5c73
carv-cmd <bencarv1324@gmail.com>
2021-12-01 14:18:10 -0500

    PreAdd:  M git-commits.sh
    PostCommit:
    M  git-commits.sh

--- git-commits.sh
+++ git-commits.sh
@@ -10,13 +10,13 @@
 
 PROGNAME=${0##*/}
 GITBASH="${HOME}/bin/gitbash"
+GIT_IGNORE="${GITBASH}/template.gitignore"
 
 Usage () {
 	cat <<- EOF
-
 ${PROGNAME} - the stupid git commit manager
 
-usage: ${PROGNAME} [ -m ] [ -q ] [ -t ]
+usage: ${PROGNAME} [ -m ] [ -q ] [ -t ] [ -D ] [ -E ]
 
 Examples:
    ${PROGNAME} [ -q ][ -t ]
@@ -24,132 +24,97 @@ Examples:
    ${PROGNAME} -q -t -m 'commitMsg'
 
 Options
- -h, --help	    Display this prompt and exit.
  -m, --message      Custom commit message.
  -p, --push         Push upstream after committing changes.
- -q, --quiet        Dont prompt before 'add -> commit'.
- -t, --templates    Create README.md & .gitignore files if doesn't exist.
+ -q, --quiet        Dont prompt before 'add && commit' (automation).
+ -t, --templates    Create README.md & .gitignore files if they dont exist.
+ -D, --dry-run      Execute git commands with --dry-run flags
+ -E, --echo         Echo commands that will be executed(debug).
+ -h, --help	    Display this prompt and exit.
 
 EOF
 exit 1
 }
 
 Error () {
-    declare -A ERR
-    ERR['token']='unknown parameter'
-    ERR['noAdd']='`git add` failed'
-    ERR['noGit']='./.git does not exist in directory'
-    ERR['noCom']='failed commit'
-    ERR['push']='git push upstream failed'
-    echo -e "\nraised: ${ERR[${1}]}"
-    unset 'ERR'
+    echo -e "error: $1"
     exit 1
+}
+
+Commit_msg () {
+    local short_stat="$(git status --short)"
+    if [[ "$STATUS" =~ ^null$ ]]; then
+        echo "$short_stat" 
+    else
+        echo -e "\nPreAdd:\n${STATUS}\nPostCommit:\n$short_stat\n"
+    fi
+}
+
+Update_index () {
+    [ ! "$STATUS" ] && return || git status --short
+
+    local REPLY=yes
+    if [ -z "${KWARGS["quiet"]}" ]; then
+        read -p '[ add && commit ](y/n)?: '
+    fi
+    case "$REPLY" in
+        y | yes )  $sh_c "git add $DRY_RUN ." || Error 'git add';;
+        * )  Error 'git add';;
+    esac
 }
 
 Template_files () {
     local CWD=$(pwd)
-    local MK_README="${CWD}/README.md"
-    local GITIGNORE="${CWD}/.gitignore"
-    local TEMPLATE="${GITBASH}/template.gitignore"
-
-    if [ ! -f "$MK_README" ]; then
-        echo "# ${CWD##*/}" >> "$MK_README"
+    if [ ! -f ./README.md ]; then
+        $sh_c "set -C; echo '# ${CWD##*/}' > ./README.md"
     fi
-    if [[ -e "$TEMPLATE" && ! -e "$GITIGNORE" ]]; then
-        cp "$GITIGNORE" .gitignore
-    fi
-}
-
-Select_add () {
-    # Yes `git commit --interactive` exists, I dont like it.
-    cat <<- 'EOF'
- * Enter filenames to `git add` from listing above.
- * One or more filenames can be passed on each line entry.
- * Pass string 'exit' to exit without making any changes.
- * Add & Commit changes with 2 blank newlines.
- * Staged command printed for confirmation before execution.
- * Duplicates are ignored; i.e. `git add x.py x.py` == `git add x.py`
-EOF
-    declare -a _ADDER
-    while true; do
-        read -a REPLY -rep "Queue:[ ${_ADDER[@]}]<<< "
-        
-        if [[ "${REPLY}" == 'exit' ]]; then
-            Error 'noAdd'
-
-        elif [[ ! "${REPLY}" ]]; then
-            echo; read -p "git add ${_ADDER[@]} <<<(y/n): "
-            [[ "${REPLY}" =~ ^(y|yes)$ ]] && break
-
-        else
-            printf '\033[0F\033[0J'
-            for _valid in "${REPLY[@]}"; do
-                if [ ! -e "$_valid" ]; then
-                    echo -e "FilenameError: '${_valid}'\n"
-                else
-                    _ADDER+="$_valid"
-                fi
-            done
-    fi
-
-    done
-    if (( ${#_ADDER} )); then 
-        git add ${_ADDER[@]}
-    else
-        Error 'noAdd'
-    fi
-}
-
-Prompt_user () {
-    git status --short; echo
-    read -p '[ add && commit ]:(y/n/[s]elect): '
-    case "$REPLY" in
-        y | yes )  git add . || Error 'noAdd';;
-        s | select )  Select_add || Error 'noAdd';;
-        * )  Error 'noAdd';;
-    esac
-}
-
-Commit_msg () {
-    if [[ -n "${_SETUP['message']}" ]];then
-        echo "${_SETUP['message']}"
-    elif [[ "${STATUS}" ]]; then	
-        echo -e "\nPreAdd:\n$STATUS\nPostCommit:\n$(git status --short)\n"
-    elif [[ ! "${STATUS}" == 'null' ]]; then
-        echo "$(git status --short)" 
+    if [[ -e "$GIT_IGNORE" && ! -e ./.gitignore ]]; then
+        $sh_c "cp $GIT_IGNORE .gitignore"
     fi
 }
 
 Main_loop () {
-    local STATUS=
-    readarray -d '\n' STATUS < <(git status --short || false) 
-    [ -z "$STATUS" ] && STATUS='null'
+    STATUS=
+    if [[ -z "$COMMIT_MSG" ]];then
+        readarray -d '\n' STATUS < <(git status --short || false) 
+        [ ! "$STATUS" ] && STATUS='null'
+    fi
 
-    (( ${_SETUP['templates']} )) && Template_files  
-    (( ${_SETUP['quiet']} )) && git add . || Prompt_user
-    git commit -m "$(Commit_msg)" 
-}
+    [ -n "${KWARGS["templates"]}" ] && Template_files
+    Update_index
 
-Parse_args () {
-    declare -A _SETUP
-    while [ -n "$1" ]; do
-        case "$1" in
-            -m | --message )  shift; _SETUP['message']="$1";;
-            -p | --push )  _SETUP['push']=1;;
-            -q | --quiet )  _SETUP['quiet']=1;;
-            -t | --templates )  _SETUP['templates']=1;;
-            -h | --help )  Usage;;
-            * )  Error 'token';;
-        esac
-        shift
-    done
-    Main_loop "$_SEUTP"
-
-    if (( ${_SETUP['push']} )); then 
-        git push origin $(git branch --show-current) || Error 'push'
+    COMMIT_MSG="$(Commit_msg $STATUS)"
+    $sh_c "git commit $DRY_RUN -m '${COMMIT_MSG}'"
+    if [ -n "${KWARGS["push"]}" ]; then
+        $sh_c "git push $DRY_RUN origin $(git branch --show-current)"
     fi
 }
 
-[ -e "$(pwd)/.git" ] || Error 'noGit'
+Parse_args () {
+    COMMIT_MSG=
+    while [ -n "$1" ]; do
+        case "$1" in
+            -m | --message )  shift; COMMIT_MSG="$1";;
+            -p | --push )  KWARGS['push']='p';;
+            -q | --quiet )  KWARGS["quiet"]='q';;
+            -t | --templates )  KWARGS['templates']='t';;
+            -D | --dry-run ) DRY_RUN='--dry-run';;
+            -E | --echo ) sh_c='echo';;
+            -h | --help )  Usage;;
+            * )  Error 'unknown token';;
+        esac
+        shift
+    done
+}
+
+
+if [ ! -d "$(pwd)/.git" ]; then
+    git status
+    exit
+fi
+
+sh_c='sh -c'
+declare -A KWARGS
 Parse_args "${@}"
+Main_loop
 
