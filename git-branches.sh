b57b86a15447da5458fda29eec5e386d508a9ee0
carv-cmd <bencarv1324@gmail.com>
2021-12-01 00:38:54 -0500

    PreAdd:  M git-branches.sh  M git-commits.sh
    PostCommit:
    M  git-branches.sh
    M  git-commits.sh

--- git-branches.sh
+++ git-branches.sh
@@ -1,146 +1,117 @@
 #!/bin/bash
 
-#        _ _   _                     _    
-#   __ _(_) |_| |__ _ _ __ _ _ _  __| |_  
-#  / _` | |  _| '_ \ '_/ _` | ' \/ _| ' \ 
-#  \__, |_|\__|_.__/_| \__,_|_||_\__|_||_|
-#  |___/                                  
-#
-# git-branches.sh: The stupid git branch manager
+#       _ _   _                  
+#  __ _(_) |_| |__ _ _ __ _ _ _  
+# / _` | |  _| '_ \ '_/ _` | ' \ 
+# \__, |_|\__|_.__/_| \__,_|_||_|
+# |___/                          
+# 
+# The stupid git branch manager
 
-MAINS='main'
-CW_BRANCH="$(git branch --show-current)"
-GIT_ORIGIN=${ORIGIN:-origin}
-GIT_BRANCH=${BRANCH:-}
+MASTER='main'
+ORIGIN=${ORIGIN:-origin}
+NEW_BRANCH=${NEW_BRANCH:-}
+CURRENT_BRANCH="$(git branch --show-current)"
+
 
 Usage () {
     PROGNAME="${0##*/}"
     cat >&2 <<- EOF
-The stupid git branch manager.
-usage: $PROGNAME [-b|--branch][-C|--ctrack][-D|--delete][-M|--merge] NAME
+usage: $PROGNAME
 
-Mode Flags:
- -a, --all      Shortcut for \`git branch --all -vv\`
- -b, --branch   Checkout new branch NAME from CW_BRANCH locally.
- -C, --ctrack   Same as --branch + git pushes branch NAME upstream.
- -D, --delete   (CAUTION) Delete all NAME local and remote branches.
- -M, --merge    Merge NAME into current working branch.
- --dry          Echo commands that will be executed.
- -h, --help     Display this help message and exit.
+ $PROGNAME [--checkout]|[--checkout-track]|[--delete]|[--merge] [--dry] BRANCH_NAME
+ $PROGNAME [-a|--all]
 
-Environment:
-============
+Options:
+ -c, --checkout         Checkout new BRANCH_NAME from CURRENT_BRANCH.
+ -C, --checkout-track   Identical to -c, but pushes BRANCH_NAME upstream.
+ -D, --delete           Delete all local and remote BRANCH_NAME's.
+ -M, --merge            Merge BRANCH_NAME into CURRENT_BRANCH.
+ -a, --all              Shortcut for \`git branch --all -vv\`
+ -d, --dry              Echo commands that would be executed.
+ -h, --help             Display this help message and exit.
+
+==========
+For defaults set the following in your shell.
+==========
 ORIGIN: 
- Used in \`git remote-ops ORIGIN/name\`.
- Can only be set through env variable.
-
+ Can only be set through environment.
 BRANCH: 
- Specifying -b, --branch overrides this.
+ Command line arguments take precedence.
 
 EOF
+exit 1
 }
 
 Error () {
-    if [ -n "$1" ]; then
-        echo -e "error: $1\n" > /dev/stderr 
-    else
-        Usage > /dev/stderr
-    fi
+    echo -e "error: $1\n" > /dev/stderr 
     exit 1
 }
 
-Branch_validator () {
-    #if git branch --list | grep -qE "^\* $GIT_BRANCH"; then
+Valid_branch () {
     local ACTION="$1"
-    if [ "$GIT_BRANCH" == "$CW_BRANCH" ]; then
-        berr="${ACTION}($GIT_BRANCH) == current($CW_BRANCH)
-        \rLocal Branches:\n`git branch --list`"
-        Error "$berr"
-    elif ! git branch --list | grep -q $GIT_BRANCH; then
-        Error "branch[ $GIT_BRANCH ]: doesn't exist"
+    if [ "$NEW_BRANCH" == "$CURRENT_BRANCH" ]; then
+        Error "$ACTION($NEW_BRANCH) == current($CURRENT_BRANCH)"
+    elif ! git branch --list | grep -q $NEW_BRANCH; then
+        Error "branch[ $NEW_BRANCH ]: doesn't exist"
     fi
 }
 
-Branch_cleanup () {
-    # Delete local branch, remote branch
-    if [[ "$GIT_BRANCH" =~ ^$MAINS$ ]]; then
-        Error 'protect: main: exiting'
-    fi
-    Branch_validator 'delete'
-    $sh_c "git branch -d $GIT_BRANCH" && 
-        $sh_c "git branch -d -r $GIT_ORIGIN/$GIT_BRANCH" &&
-        $sh_c "git push $GIT_ORIGIN --delete $GIT_BRANCH"
+Stale_cleaner () {
+    # Delete all local and remote references to stale branch.
+    Valid_branch 'delete'
+    $sh_c "git branch -d $NEW_BRANCH" && 
+        $sh_c "git branch -d -r $ORIGIN/$NEW_BRANCH" &&
+        $sh_c "git push $ORIGIN --delete $NEW_BRANCH"
 }
 
-Branch_merge () {
-    Branch_validator 'merge'
-    if $sh_c "git merge $GIT_BRANCH"; then
-        if $sh_c "git push $GIT_ORIGIN $CW_BRANCH"; then
-            read -p "Reset: $GIT_BRANCH: (y/n)?: "
-            if [[ "$REPLY" =~ ^(y|yes)$ ]]; then
-                Branch_chkout_track '--track'
-            fi
+Merger () {
+    Valid_branch 'merge'
+    if $sh_c "git merge $NEW_BRANCH"; then
+        if git branch --list -r | grep -q ^*$NEW_BRANCH*$; then
+            $sh_c "git push $ORIGIN $CURRENT_BRANCH" && New_branch '--track'
+        else
+            New_branch
         fi
     fi
 }
 
-Branch_chkout_track () {
-    # Checkout $GIT_BRANCH from $CW_BRANCH.
+New_branch () {
     local TRACK=${1:-}
-    if $sh_c "git checkout -B $GIT_BRANCH $TRACK"; then
+    if $sh_c "git checkout -B $NEW_BRANCH $TRACK"; then
         if [ "$TRACK" ]; then
-            $sh_c "git push $GIT_ORIGIN $GIT_BRANCH"
+            $sh_c "git push $ORIGIN $NEW_BRANCH"
         fi
-    fi
-}
-
-Set_branch () {
-    # $BRANCH name can only be set once.
-    if [ ! "$1" ]; then
-        return 1
-    elif [[ "$1" =~ ^-(o|b|C|D|\-origin|\-branch|\-ctrack|\-delete)$ ]]; then
-        Error "Flag given as argument: $1 ?"
-    elif [ ! "$GIT_BRANCH" ]; then
-        GIT_BRANCH="$1"
     fi
 }
 
 Parse_args () {
-    local iflag=
-    while [ -n "$1" ]; do
-        iflag="$1"; shift
-        case "$iflag" in 
+    for arg in "$@"; do
+        case "$arg" in 
             -a | --all )  git branch --all -vv; exit;;
-            -b | --branch )  MODE='checkout';;
-            -C | --ctrack )  MODE='ctrack';;
+            -c | --checkout )  MODE='checkout';;
+            -C | --checkout-track )  MODE='ctrack';;
             -D | --delete )  MODE='delete';;
             -M | --merge )  MODE='merge';;
-            --dry )  sh_c='echo'; continue;;
-            -h | --help )  Error;;
-            * )  Error 'invalid argument';;
+            -d | --dry )  sh_c='echo'; continue;;
+            -h | --help )  Usage > /dev/stderr;;
+            * )  [[ ! "$arg" =~ ^--?[A-Za-z]+$ ]] && 
+                NEW_BRANCH="$arg";;
         esac
-        Set_branch "$1" && shift
     done
-
-    local BRANCH_REGEX='^([[:alnum:]]|\-|\_|\.)+$'
-    if [[ "$GIT_BRANCH" =~ ^--(dry|all)$ ]]; then
-        Error 'NAME == --dry?'
-    elif [ ! "$GIT_BRANCH" ]; then 
-        Error 'No branch name given'
-    elif [[ ! "$GIT_BRANCH" =~ $BRANCH_REGEX ]]; then
-        Error "regex: $GIT_BRANCH !match: $BRANCH_REGEX"
+    if [ ! "$NEW_BRANCH" ]; then
+        Error 'branch name unset.'
     fi
 }
 
-sh_c='sh -c'
 MODE=
-if Parse_args "$@"; then
-    case "$MODE" in
-        checkout )  Branch_chkout_track;;
-        ctrack )  Branch_chkout_track '--track';;
-        delete ) Branch_cleanup;;
-        merge )  Branch_merge;;
-        * ) Error "FatalUnexpected";;
-    esac
-fi
+sh_c='sh -c'
+Parse_args "$@"
+case "$MODE" in
+    checkout )  New_branch;;
+    ctrack )  New_branch '--track';;
+    delete ) Stale_cleaner;;
+    merge )  Merger;;
+    * ) Error "UnexpectedError";;
+esac
 
--- git-commits.sh
+++ git-commits.sh
@@ -23,174 +23,133 @@ Examples:
    ${PROGNAME} -t -m 'commitMsg'
    ${PROGNAME} -q -t -m 'commitMsg'
 
- Flags:
-   -h | --help		= display this prompt.
-   -m | --cmsg		= commit message (git commit -m '').
-   -p | --push		= push upstream after committing changes.
-   -q | --quiet		= dont prompt before 'add -> commit' (use wisely).
-   -t | --templates	= create README.md & .gitignore files if doesn't exist.
+Options
+ -h, --help	    Display this prompt and exit.
+ -m, --message      Custom commit message.
+ -p, --push         Push upstream after committing changes.
+ -q, --quiet        Dont prompt before 'add -> commit'.
+ -t, --templates    Create README.md & .gitignore files if doesn't exist.
 
 EOF
 exit 1
 }
 
-Prog_error () {
-	declare -A ERR
-	ERR['token']='unknown parameter'
-	ERR['noAdd']='`git add` failed'
-	ERR['noGit']='./.git does not exist in directory'
-	ERR['noCom']='failed commit'
-	ERR['push']='git push upstream failed'
-	echo -e "\nraised: ${ERR[${1}]}"
-	unset 'ERR'
-	exit 1
+Error () {
+    declare -A ERR
+    ERR['token']='unknown parameter'
+    ERR['noAdd']='`git add` failed'
+    ERR['noGit']='./.git does not exist in directory'
+    ERR['noCom']='failed commit'
+    ERR['push']='git push upstream failed'
+    echo -e "\nraised: ${ERR[${1}]}"
+    unset 'ERR'
+    exit 1
 }
 
 Template_files () {
-	# Generate template.gitignore then save in $GITBASH directory.
-	# Create basic 'README.md' and '.gitignore' files if they dont exist.
-	local CWD=$(pwd)
-	local MK_README="${CWD}/README.md"
-	local IGNORE_TEMPLATE="${CWD}/.gitignore"
-	local GITIGNORE="${GITBASH}/template.gitignore"
+    local CWD=$(pwd)
+    local MK_README="${CWD}/README.md"
+    local GITIGNORE="${CWD}/.gitignore"
+    local TEMPLATE="${GITBASH}/template.gitignore"
 
-	[ ! -e "${MK_README}" ] &&
-		echo "# ${CWD##*/}" >> "${MK_README}"
-
-	[[ ! -e "${IGNORE_TEMPLATE}" && -e "${GITIGNORE}" ]] && 
-		cp "${GITIGNORE}" .gitignore
+    if [ ! -f "$MK_README" ]; then
+        echo "# ${CWD##*/}" >> "$MK_README"
+    fi
+    if [[ -e "$TEMPLATE" && ! -e "$GITIGNORE" ]]; then
+        cp "$GITIGNORE" .gitignore
+    fi
 }
 
 Select_add () {
-	# I know `git commit --interactive` exists
-	cat <<- 'EOF'
+    # Yes `git commit --interactive` exists, I dont like it.
+    cat <<- 'EOF'
+ * Enter filenames to `git add` from listing above.
+ * One or more filenames can be passed on each line entry.
+ * Pass string 'exit' to exit without making any changes.
+ * Add & Commit changes with 2 blank newlines.
+ * Staged command printed for confirmation before execution.
+ * Duplicates are ignored; i.e. `git add x.py x.py` == `git add x.py`
+EOF
+    declare -a _ADDER
+    while true; do
+        read -a REPLY -rep "Queue:[ ${_ADDER[@]}]<<< "
+        
+        if [[ "${REPLY}" == 'exit' ]]; then
+            Error 'noAdd'
 
-	* Enter filenames to `git add` from listing above.
-	* One or more filenames can be passed on each line entry.
-	* Pass string 'exit' to exit without making any changes.
-	* Add & Commit changes with 2 blank newlines.
-	* Staged command printed for confirmation before execution.
-	* Duplicates are ignored; i.e. `git add x.py x.py` == `git add x.py`
+        elif [[ ! "${REPLY}" ]]; then
+            echo; read -p "git add ${_ADDER[@]} <<<(y/n): "
+            [[ "${REPLY}" =~ ^(y|yes)$ ]] && break
 
-	EOF
-	declare -a _ADDER
-	while true; do
-		read -a REPLY -rep "Queue:[ ${_ADDER[@]}]<<< "
-		
-		if [[ "${REPLY}" == 'exit' ]]; then
-			Prog_error 'noAdd'
+        else
+            printf '\033[0F\033[0J'
+            for _valid in "${REPLY[@]}"; do
+                if [ ! -e "$_valid" ]; then
+                    echo -e "FilenameError: '${_valid}'\n"
+                else
+                    _ADDER+="$_valid"
+                fi
+            done
+    fi
 
-		elif [[ ! "${REPLY}" ]]; then
-			echo; read -p "git add ${_ADDER[@]} <<<(y/n): "
-			[[ "${REPLY}" =~ ^(y|yes)$ ]] && break
-
-		else
-			printf '\033[0F\033[0J'
-			for _valid in "${REPLY[@]}"; do
-				[ -e "${_valid}" ] && 
-					_ADDER+="${_valid} " || 
-					echo -e "FilenameError: '${_valid}'\n"
-			done
-		fi
-
-	done
-	if (( ${#_ADDER} )); then 
-		git add ${_ADDER[@]}
-		unset '_ADDER' '_EXIT'
-	else
-		Prog_error 'noAdd'
-	fi
+    done
+    if (( ${#_ADDER} )); then 
+        git add ${_ADDER[@]}
+    else
+        Error 'noAdd'
+    fi
 }
 
 Prompt_user () {
-	# Ask-before-add-&-commit enabled by default.
-	# (y|yes) commits everything listed by git status.
-	# (s|select) lets user select specific files to add/commit.
-	# Calling from scripts, bypass with '-q | --quiet' option.
-	git status --short; echo
-	local _PROMPT='[ add -> commit ]:(y/n/[s]elect): '
-	read -p "${_PROMPT}" _response
-	case "${_response}" in
-		y | yes ) 
-			git add . || 
-				Prog_error 'noAdd'
-			;;
-		s | select )
-			Select_add ||  # See Select_add '<<- here string'
-				Prog_error 'noAdd'
-			;;
-		* )
-			Prog_error 'noAdd'
-			;;
-	esac
+    git status --short; echo
+    read -p '[ add && commit ]:(y/n/[s]elect): '
+    case "$REPLY" in
+        y | yes )  git add . || Error 'noAdd';;
+        s | select )  Select_add || Error 'noAdd';;
+        * )  Error 'noAdd';;
+    esac
+}
+
+Commit_msg () {
+    if [[ -n "${_SETUP['message']}" ]];then
+        echo "${_SETUP['message']}"
+    elif [[ "${STATUS}" ]]; then	
+        echo -e "\nPreAdd:\n$STATUS\nPostCommit:\n$(git status --short)\n"
+    elif [[ ! "${STATUS}" == 'null' ]]; then
+        echo "$(git status --short)" 
+    fi
 }
 
 Main_loop () {
-	local STATUS=  # Store short status for auto commit msg.
-	readarray -d '\n' STATUS < <(git status --short || false) 
-	[[ ! ${STATUS} ]] && STATUS='null'
+    local STATUS=
+    readarray -d '\n' STATUS < <(git status --short || false) 
+    [ -z "$STATUS" ] && STATUS='null'
 
-	# See `Template_files` comments.
-	(( ${_SETUP['templates']} )) && 
-		Template_files  
-	
-	# Pass --quiet option from scripts to silently git add.
-	(( ${_SETUP['quiet']} )) &&
-		git add . || Prompt_user  # See `Prompt_user` comments.
-
-	# Generate pre -> post state commit msg if none supplied by user.
-	local COMMIT_MSG="${_SETUP['cmsg']}"
-
-	# Commit changes.
-	if [[ -z "${COMMIT_MSG}" ]];then
-		if [[ "${STATUS}" ]]; then	
-			printf -v COMMIT_MSG "\nPreCommit:\n%s\nPostCommit:\n%s\n" \
-				"${STATUS}" "$(git status --short)"
-
-		elif [[ ! "${STATUS}" == 'null' ]]; then
-			COMMIT_MSG="$(git status --short)" 
-		fi
-	fi
-	git commit -m "${COMMIT_MSG}" 
+    (( ${_SETUP['templates']} )) && Template_files  
+    (( ${_SETUP['quiet']} )) && git add . || Prompt_user
+    git commit -m "$(Commit_msg)" 
 }
 
 Parse_args () {
-	# Parse user input & populate hash table for Main_loop.
-	declare -A _SETUP
-	while [ -n "${1}" ]; do
-		case "${1}" in
-			-m | --cmsg )
-				shift
-				_SETUP['cmsg']="${1}"
-				;;
-			-p | --push )
-				_SETUP['push']=1
-				;;
-			-q | --quiet )
-				_SETUP['quiet']=1
-				;;
-			-t | --templates )
-				_SETUP['templates']=1
-				;;
-			-h | --help )
-				Usage
-				;;
-			* ) 
-				Prog_error 'token'
-				;;
-		esac
-		shift
-	done
-	Main_loop "${_SEUTP}"
+    declare -A _SETUP
+    while [ -n "$1" ]; do
+        case "$1" in
+            -m | --message )  shift; _SETUP['message']="$1";;
+            -p | --push )  _SETUP['push']=1;;
+            -q | --quiet )  _SETUP['quiet']=1;;
+            -t | --templates )  _SETUP['templates']=1;;
+            -h | --help )  Usage;;
+            * )  Error 'token';;
+        esac
+        shift
+    done
+    Main_loop "$_SEUTP"
 
-	# Push upstream if -p flag has been set.
-	if (( ${_SETUP['push']} )); then 
-            git push origin $(git branch --show-current) || Prog_error 'push'
-	fi
+    if (( ${_SETUP['push']} )); then 
+        git push origin $(git branch --show-current) || Error 'push'
+    fi
 }
 
-# Verify git history exists in CWD, otherwise exit 1.
-[ -e "$(pwd)/.git" ] && 
-	Parse_args "${@}" ||
-	Prog_error 'noGit'
+[ -e "$(pwd)/.git" ] || Error 'noGit'
+Parse_args "${@}"
 
