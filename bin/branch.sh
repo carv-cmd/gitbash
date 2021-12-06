f4aaf50de1ef1c30861e5190202aecd740886a20
carv-cmd <bencarv1324@gmail.com>
2021-12-06 18:36:57 -0500

    ChangeLog: M  bin/branch.sh

--- bin/branch.sh
+++ bin/branch.sh
@@ -2,6 +2,8 @@
 
 # The stupid git branch manager
 
+PROGNAME="${0##*/}"
+
 if ! ON_BRANCH="$(git branch --show-current)"; then
     exit 1
 elif [[ "$1" =~ ^-(a|\-all)$ ]]; then
@@ -12,30 +14,32 @@ sh_c='sh -c'
 ECHO=${ECHO:-}
 [ "$ECHO" ] && sh_c='echo'
 
-MASTER='main'
 ORIGIN=${ORIGIN:-origin}
 BRANCH_TASK="$1"
 BRANCH_NAME="$2"
+OK_RESET='b'
 
 
 Usage () {
-    PROGNAME="${0##*/}"
     cat >&2 <<- EOF
-usage: $PROGNAME 
-
- $PROGNAME [-a|--all]
- $PROGNAME [-m|--merge] BRANCH_NAME
- $PROGNAME [-l|--mk-locals]|[-L|--rm-locals] BRANCH_NAME
- $PROGNAME [-r|--mk-remotes]|[-R|--rm-remotes] BRANCH_NAME
+The stupid git branch manager
+usage: 
+ $PROGNAME [ -a | --all ]
+ $PROGNAME [ -l | --local ] | [ -r | --remote ] BRANCH_NAME
+ $PROGNAME [ -m | --merge ] | [ -M ] BRANCH_NAME
+ $PROGNAME [ -d | --delete ] | [ -D ] BRANCH_NAME
 
 Options:
- -l, --mk-locals        Checkout new BRANCH_NAME from ON_BRANCH.
- -r, --mk-remotes       Identical to -l, but pushes BRANCH_NAME upstream.
- -m, --merge            Merge BRANCH_NAME into ON_BRANCH.
- -L, --rm-locals        Delete local references to BRANCH_NAME
- -R, --rm-remotes       Delete remote reference to BRANCH_NAME.
- -a, --all              Print \`git branch --all -vv\` and exit.
- -h, --help             Display this help message and exit.
+ -a, --all          Print all local and remote branches for PWD.
+ -l, --local        Checkout BRANCH_NAME from current branch.
+ -L                 Force reset/checkout existing BRANCH_NAME.
+ -r, --remote       Run [ --local ] and attempt to push BRANCH_NAME upstream.
+ -R                 Force reset/checkout of BRANCH_NAME and attempt push upstream.
+ -m, --merge        Locally merge BRANCH_NAME into current branch.
+ -M                 Run [ --merge ] and attempt to publish changes upstream.
+ -d, --delete       Delete LOCAL references to BRANCH_NAME
+ -D                 Delete LOCAL and REMOTE references to BRANCH_NAME.
+ -h, --help         Display this help message and exit.
 
 =============================================
 Environment
@@ -45,69 +49,90 @@ ORIGIN:
 BRANCH: 
  Command line arguments take precedence.
 ECHO:
- Set true to echo cmds instead of executing them.
- ex. \`ECHO=true $PROGNAME -mk-remotes BRANCH_NAME\`
+ Set to echo commands instead of executing them.
+ ex. \`ECHO=1 $PROGNAME -M BRANCH_NAME\`
 
 EOF
-exit
+exit 1
 }
 
 Error () {
-    echo -e "error: $1\n" > /dev/stderr 
+    echo -e "-${PROGNAME%.*}: error: $1\n" > /dev/stderr 
     exit 1
 }
 
-execute_branch_task () {
-    case "$BRANCH_TASK" in 
-        -l | --mk-locals )  make_locals;;
-        -r | --mk-remotes ) make_remotes;; 
-        -m | --merge )  merge_upstream;;
-        -L | --rm-locals ) remove_locals;;
-        -R | --rm-remotes )  remove_remotes;;
-        -h | --help )  Usage;;
-        * ) Error "unknown option: $BRANCH_TASK"
-    esac
-}
-
-make_locals () {
+make_new_branch () {
     local TRACK=${1:-}
-    $sh_c "git checkout -B $BRANCH_NAME $TRACK"
-}
-
-make_remotes () {
-    if make_locals '--track'; then
-        $sh_c "git push $ORIGIN $BRANCH_NAME"
+    if ! $sh_c "git checkout -$OK_RESET $BRANCH_NAME $TRACK"; then
+        Error "use [ -L | -R ] or [ --help ]"
     fi
 }
 
-send_upstream () {
-    if [ -n "$(git remote -v)" ]; then
-        $sh_c "git push $ORIGIN $ON_BRANCH"
+make_remotes () {
+    if make_new_branch '--track'; then
+        push_upstream "$BRANCH_NAME"
+    fi
+}
+
+merge_local () {
+    if ! $sh_c "git merge $BRANCH_NAME"; then
+        Error 'git merge: failed'
     fi
 }
 
 merge_upstream () {
-    if $sh_c "git merge $BRANCH_NAME"; then
-        send_upstream
-    fi
+    merge_local
+    BRANCH_NAME="$ON_BRANCH" 
+    push_upstream
 }
 
 remove_locals () {
-    $sh_c "git branch -d $BRANCH_NAME" 
+    if ! $sh_c "git branch -d $BRANCH_NAME"; then
+        Error 'remove locals failed'
+    fi
 }
 
 remove_remotes () {
-    if [ -n "$(git remtote -v)" ]; then
-        $sh_c "git branch -d -r $ORIGIN/$BRANCH_NAME"
+    check_remote_origin
+    remove_locals
+    if $sh_c "git branch -d -r $ORIGIN/$BRANCH_NAME"; then
         $sh_c "git push $ORIGIN --delete $BRANCH_NAME"
     fi
 }
 
-if [ ! "$BRANCH_NAME" ]; then
-    Usage > /dev/stderr
+push_upstream () {
+    check_remote_origin
+    $sh_c "git push $ORIGIN $BRANCH_NAME"
+}
+
+check_remote_origin () {
+    if [ -z "$(git remote -v)" ]; then
+        Error 'no remote origin'
+    fi
+}
+
+verbose_branches () {
+    git branch --all -vv
+}
+
+
+if [[ "$BRANCH_TASK" =~ ^--?(h(elp)?|a(ll)?)$ ]]; then
+    OK_RESET=
 elif [[ ! "$BRANCH_NAME" =~ ^[[:alnum:]](\-|\_|\.|[[:alnum:]])*[a-z0-9]$ ]]; then
-    Error "invalid branch_name: $BRANCH_NAME"
+    Error "invalid_name: [ '$BRANCH_NAME' ]"
 fi
 
-execute_branch_task
+case "$BRANCH_TASK" in 
+    -a | --all ) verbose_branches;;
+    -l | --local ) make_new_branch;;
+    -L ) OK_RESET='B' make_new_branch;;
+    -r | --remote ) make_remotes;;
+    -R ) OK_RESET='B' make_remotes;;
+    -m | --merge ) merge_local;;
+    -M ) merge_upstream;;
+    -d | --delete ) remove_locals;;
+    -D ) remove_remotes;;
+    -h | --help ) Usage;;
+    * ) Error "unknown option: $BRANCH_TASK";;
+esac
 
